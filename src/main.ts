11d8eaa6c53547281dbd89ee0996056c09f37bcf
Andres12345-debug <julian2000mk1@gmail.com>
2026-02-11 22:02:25 -0500

    Nueva forma de llamar el puerto

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-    const puerto = Number(process.env.PUERTO_SERVIDOR);
+  const puerto = Number(process.env.PORT) || 3000; // 👈 fallback seguro
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.enableCors();
-    // Configurar servir archivos estáticos desde la carpeta uploads, MAQUINA LOCAL
-    app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-        prefix: '/uploads/'
-    });
-    await app.listen(puerto, () => {
-        console.log(`Servidor funcionando puerto: ` + puerto);
-    });
+  app.enableCors();
+
+  // Servir archivos estáticos
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  await app.listen(puerto);
+  console.log(`Servidor funcionando en puerto: ${puerto}`);
 }
 bootstrap();
-
